3f911b69ba053b7e774b425e5bb4b7a0c09ea582
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2024-04-27 01:13:54 +0600

    Client side to send to server side

--- index.js
+++ index.js
@@ -24,13 +24,26 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const craftCollection = client.db("craftDB").collection('craft');
+
+    //send add craft item to the server
+    app.post('/addCrafts', async(req, res) => {
+      const newCraftItem = req.body;
+      console.log(newCraftItem);
+      const result = await craftCollection.insertOne(newCraftItem);
+      res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
